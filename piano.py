4432f543743440cb79ff005ad98febb95d22057a
SalmaAtia <salmaati200@gmail.com>
2020-05-16 00:36:15 +0200

    digital and regular piano done but final changes are needed

--- piano.py
+++ piano.py
@@ -7,44 +7,67 @@ import numpy as np
 import random
 from scipy.io import wavfile
 import simpleaudio as sa
-
+import music21
+from music21 import note, stream, midi
 
 class ApplicationWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(ApplicationWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        # self.ui.araf1.clicked.connect(self.NOIDEA)
-        freqs=[261.63 , 277.18, 293.66, 311.13, 329.63, 349.23, 369.99, 392.00, 415.30,440.00, 466.16, 493.88,523.25,554.37,587.33,622.25, 659.25, 698.46, 739.99, 783.99, 830.61,880.00, 932.33,987.77]
-        notes=[self.ui.c4,self.ui.c40,self.ui.d4,self.ui.d40,self.ui.e4,self.ui.f4,self.ui.f40,self.ui.g4,self.ui.g40,self.ui.a4,self.ui.a40,self.ui.b4,self.ui.c5,self.ui.c50,self.ui.d5,self.ui.d50,self.ui.e5,self.ui.f5,self.ui.f50,self.ui.g5,self.ui.g50,self.ui.a5,self.ui.a50,self.ui.b5]
-        for i in range(len(freqs)):
-            notes[i].clicked.connect(self.noteFunc(freqs[i]))
-        # self.ui.c4.clicked.connect(self.noteFunc(freqs[0]))
+        self.c='C4'
+        # self.ui.c4.clicked.connect(lambda: self.musictwo(self.c))
+     
+        self.freqs=[261.63 , 277.18, 293.66, 311.13, 329.63, 349.23, 369.99, 392.00, 415.30,440.00, 466.16, 493.88,523.25,554.37,587.33,622.25, 659.25, 698.46, 739.99, 783.99, 830.61,880.00, 932.33,987.77]
+        self.notes=['C4', 'C#4', 'D4',"D#4","E4","F4","F#4","G4","G#4","A4","A#4","B4","C5","C#5","D5","D#5","E5","F5","F#5","G5","G#5","A5","A#5","B5"]
+        self.buttons=[self.ui.c4,self.ui.c40,self.ui.d4,self.ui.d40,self.ui.e4,self.ui.f4,self.ui.f40,self.ui.g4,self.ui.g40,self.ui.a4,self.ui.a40,self.ui.b4,self.ui.c5,self.ui.c50,self.ui.d5,self.ui.d50,self.ui.e5,self.ui.f5,self.ui.f50,self.ui.g5,self.ui.g50,self.ui.a5,self.ui.a50,self.ui.b5]
+        self.ui.comboBox.currentTextChanged.connect(self.detectChange)
+        
        
    
 
-    def noteFunc(self,f):
-        # A_freq = f
+    def detectChange(self):
+       for i in range(len(self.notes)):
+            if self.ui.comboBox.currentText()=="Regular Piano":
+                self.normalPiano(i)
+            elif self.ui.comboBox.currentText()=="Digital Piano":
+            # for i in range(len(self.notes)):
+                self.digitalPiano(i)
+
+
+
+    def normalPiano(self,i):
+        self.buttons[i].clicked.connect(lambda: self.regularPiano(self.notes[i]))
+
+    def digitalPiano(self,i):
+        self.buttons[i].clicked.connect(lambda: self.digitalSound(self.freqs[i]))
+
+    def digitalSound(self,f):
         sample_rate = 44100
         T = 0.25
         t = np.linspace(0, T, T * sample_rate, False)
         A_note = np.sin(f * t * 2 * np.pi)
         audio = np.hstack((A_note))
-        # normalize to 16-bit range
         audio *= 32767 / np.max(np.abs(audio))
-        # # convert to 16-bit data
         audio = audio.astype(np.int16)
-        print(type(audio))
-        # start playback
         play_obj = sa.play_buffer(audio, 1, 2, sample_rate)
-        # print(type(play_obj))
         play_obj.wait_done()
+
+
+
+    def regularPiano(self,n):
+        note1=note.Note(n)
+        stream1 = stream.Stream()
+        stream1.append(note1)
+        sp = midi.realtime.StreamPlayer(stream1)
+        sp.play()
+
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
     application = ApplicationWindow()
     application.show()
-    sys.exit(app.exec_())
+    app.exec_()
 
 if __name__ == "__main__":
     main()
--- problem3.py
+++ problem3.py
@@ -12,144 +12,141 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(800, 600)
+        MainWindow.setStyleSheet("background-color: rgb(45, 45, 45);\n"
+"background-color: rgb(0, 0, 0);")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.c4 = QtWidgets.QPushButton(self.centralwidget)
         self.c4.setGeometry(QtCore.QRect(80, 100, 41, 231))
-        self.c4.setStyleSheet("background-color: rgb(242, 242, 242);\n"
-"background-color: qlineargradient(spread:pad, x1:1, y1:0.711, x2:0.903455, y2:0.711, stop:0 rgba(0, 0, 0, 255), stop:1 rgba(255, 255, 255, 255));\n"
+        self.c4.setStyleSheet("background-color: rgb(255, 255, 255);\n"
 "")
         self.c4.setObjectName("c4")
         self.d4 = QtWidgets.QPushButton(self.centralwidget)
         self.d4.setGeometry(QtCore.QRect(120, 100, 41, 231))
-        self.d4.setStyleSheet("background-color: rgb(242, 242, 242);\n"
-"background-color: qlineargradient(spread:pad, x1:1, y1:0.711, x2:0.903455, y2:0.711, stop:0 rgba(0, 0, 0, 255), stop:1 rgba(255, 255, 255, 255));\n"
-"")
+        self.d4.setStyleSheet("background-color: rgb(255, 255, 255);")
         self.d4.setObjectName("d4")
         self.e4 = QtWidgets.QPushButton(self.centralwidget)
         self.e4.setGeometry(QtCore.QRect(160, 100, 41, 231))
-        self.e4.setStyleSheet("background-color: rgb(242, 242, 242);\n"
-"background-color: qlineargradient(spread:pad, x1:1, y1:0.711, x2:0.903455, y2:0.711, stop:0 rgba(0, 0, 0, 255), stop:1 rgba(255, 255, 255, 255));\n"
-"")
+        self.e4.setStyleSheet("background-color: rgb(255, 255, 255);")
         self.e4.setObjectName("e4")
         self.f4 = QtWidgets.QPushButton(self.centralwidget)
         self.f4.setGeometry(QtCore.QRect(200, 100, 41, 231))
-        self.f4.setStyleSheet("background-color: rgb(242, 242, 242);\n"
-"background-color: qlineargradient(spread:pad, x1:1, y1:0.711, x2:0.903455, y2:0.711, stop:0 rgba(0, 0, 0, 255), stop:1 rgba(255, 255, 255, 255));\n"
-"")
+        self.f4.setStyleSheet("background-color: rgb(255, 255, 255);")
         self.f4.setObjectName("f4")
         self.g4 = QtWidgets.QPushButton(self.centralwidget)
         self.g4.setGeometry(QtCore.QRect(240, 100, 41, 231))
-        self.g4.setStyleSheet("background-color: rgb(242, 242, 242);\n"
-"background-color: qlineargradient(spread:pad, x1:1, y1:0.711, x2:0.903455, y2:0.711, stop:0 rgba(0, 0, 0, 255), stop:1 rgba(255, 255, 255, 255));\n"
+        self.g4.setStyleSheet("background-color: rgb(255, 255, 255);\n"
 "")
         self.g4.setObjectName("g4")
         self.a4 = QtWidgets.QPushButton(self.centralwidget)
         self.a4.setGeometry(QtCore.QRect(280, 100, 41, 231))
-        self.a4.setStyleSheet("background-color: rgb(242, 242, 242);\n"
-"background-color: qlineargradient(spread:pad, x1:1, y1:0.711, x2:0.903455, y2:0.711, stop:0 rgba(0, 0, 0, 255), stop:1 rgba(255, 255, 255, 255));\n"
-"")
+        self.a4.setStyleSheet("background-color: rgb(255, 255, 255);")
         self.a4.setObjectName("a4")
         self.b4 = QtWidgets.QPushButton(self.centralwidget)
         self.b4.setGeometry(QtCore.QRect(320, 100, 41, 231))
-        self.b4.setStyleSheet("background-color: rgb(242, 242, 242);\n"
-"background-color: qlineargradient(spread:pad, x1:1, y1:0.711, x2:0.903455, y2:0.711, stop:0 rgba(0, 0, 0, 255), stop:1 rgba(255, 255, 255, 255));\n"
-"")
+        self.b4.setStyleSheet("background-color: rgb(255, 255, 255);")
         self.b4.setObjectName("b4")
         self.c5 = QtWidgets.QPushButton(self.centralwidget)
         self.c5.setGeometry(QtCore.QRect(360, 100, 41, 231))
-        self.c5.setStyleSheet("background-color: rgb(242, 242, 242);\n"
-"background-color: qlineargradient(spread:pad, x1:1, y1:0.711, x2:0.903455, y2:0.711, stop:0 rgba(0, 0, 0, 255), stop:1 rgba(255, 255, 255, 255));\n"
-"")
+        self.c5.setStyleSheet("background-color: rgb(255, 255, 255);")
         self.c5.setObjectName("c5")
         self.d5 = QtWidgets.QPushButton(self.centralwidget)
         self.d5.setGeometry(QtCore.QRect(400, 100, 41, 231))
-        self.d5.setStyleSheet("background-color: rgb(242, 242, 242);\n"
-"background-color: qlineargradient(spread:pad, x1:1, y1:0.711, x2:0.903455, y2:0.711, stop:0 rgba(0, 0, 0, 255), stop:1 rgba(255, 255, 255, 255));\n"
-"")
+        self.d5.setStyleSheet("background-color: rgb(255, 255, 255);")
         self.d5.setObjectName("d5")
         self.e5 = QtWidgets.QPushButton(self.centralwidget)
         self.e5.setGeometry(QtCore.QRect(440, 100, 41, 231))
-        self.e5.setStyleSheet("background-color: rgb(242, 242, 242);\n"
-"background-color: qlineargradient(spread:pad, x1:1, y1:0.711, x2:0.903455, y2:0.711, stop:0 rgba(0, 0, 0, 255), stop:1 rgba(255, 255, 255, 255));\n"
+        self.e5.setStyleSheet("background-color: rgb(255, 255, 255);\n"
 "")
         self.e5.setObjectName("e5")
         self.f5 = QtWidgets.QPushButton(self.centralwidget)
         self.f5.setGeometry(QtCore.QRect(480, 100, 41, 231))
-        self.f5.setStyleSheet("background-color: rgb(242, 242, 242);\n"
-"background-color: qlineargradient(spread:pad, x1:1, y1:0.711, x2:0.903455, y2:0.711, stop:0 rgba(0, 0, 0, 255), stop:1 rgba(255, 255, 255, 255));\n"
-"")
+        self.f5.setStyleSheet("background-color: rgb(255, 255, 255);")
         self.f5.setObjectName("f5")
         self.g5 = QtWidgets.QPushButton(self.centralwidget)
         self.g5.setGeometry(QtCore.QRect(520, 100, 41, 231))
-        self.g5.setStyleSheet("background-color: rgb(242, 242, 242);\n"
-"background-color: qlineargradient(spread:pad, x1:1, y1:0.711, x2:0.903455, y2:0.711, stop:0 rgba(0, 0, 0, 255), stop:1 rgba(255, 255, 255, 255));\n"
-"")
+        self.g5.setStyleSheet("background-color: rgb(255, 255, 255);")
         self.g5.setObjectName("g5")
         self.a5 = QtWidgets.QPushButton(self.centralwidget)
         self.a5.setGeometry(QtCore.QRect(560, 100, 41, 231))
-        self.a5.setStyleSheet("background-color: rgb(242, 242, 242);\n"
-"background-color: qlineargradient(spread:pad, x1:1, y1:0.711, x2:0.903455, y2:0.711, stop:0 rgba(0, 0, 0, 255), stop:1 rgba(255, 255, 255, 255));\n"
-"")
+        self.a5.setStyleSheet("background-color: rgb(255, 255, 255);")
         self.a5.setObjectName("a5")
         self.b5 = QtWidgets.QPushButton(self.centralwidget)
         self.b5.setGeometry(QtCore.QRect(600, 100, 41, 231))
-        self.b5.setStyleSheet("background-color: rgb(242, 242, 242);\n"
-"background-color: qlineargradient(spread:pad, x1:1, y1:0.711, x2:0.903455, y2:0.711, stop:0 rgba(0, 0, 0, 255), stop:1 rgba(255, 255, 255, 255));\n"
-"")
+        self.b5.setStyleSheet("background-color: rgb(255, 255, 255);")
         self.b5.setObjectName("b5")
         self.c40 = QtWidgets.QPushButton(self.centralwidget)
         self.c40.setGeometry(QtCore.QRect(100, 100, 31, 111))
         self.c40.setStyleSheet("background-color: rgb(0, 0, 0);\n"
-"background-color: qlineargradient(spread:pad, x1:0.028, y1:0.619, x2:1, y2:0.494, stop:0.852273 rgba(0, 0, 0, 250), stop:1 rgba(255, 255, 255, 255));")
+"")
         self.c40.setObjectName("c40")
         self.d40 = QtWidgets.QPushButton(self.centralwidget)
         self.d40.setGeometry(QtCore.QRect(150, 100, 31, 111))
         self.d40.setStyleSheet("background-color: rgb(0, 0, 0);\n"
-"background-color: qlineargradient(spread:pad, x1:0.028, y1:0.619, x2:1, y2:0.494, stop:0.852273 rgba(0, 0, 0, 250), stop:1 rgba(255, 255, 255, 255));")
+"")
         self.d40.setObjectName("d40")
         self.f40 = QtWidgets.QPushButton(self.centralwidget)
         self.f40.setGeometry(QtCore.QRect(220, 100, 31, 111))
         self.f40.setStyleSheet("background-color: rgb(0, 0, 0);\n"
-"background-color: qlineargradient(spread:pad, x1:0.028, y1:0.619, x2:1, y2:0.494, stop:0.852273 rgba(0, 0, 0, 250), stop:1 rgba(255, 255, 255, 255));")
+"")
         self.f40.setObjectName("f40")
         self.g40 = QtWidgets.QPushButton(self.centralwidget)
         self.g40.setGeometry(QtCore.QRect(260, 100, 31, 111))
         self.g40.setStyleSheet("background-color: rgb(0, 0, 0);\n"
-"background-color: qlineargradient(spread:pad, x1:0.028, y1:0.619, x2:1, y2:0.494, stop:0.852273 rgba(0, 0, 0, 250), stop:1 rgba(255, 255, 255, 255));")
+"")
         self.g40.setObjectName("g40")
         self.a40 = QtWidgets.QPushButton(self.centralwidget)
         self.a40.setGeometry(QtCore.QRect(300, 100, 31, 111))
         self.a40.setStyleSheet("background-color: rgb(0, 0, 0);\n"
-"background-color: qlineargradient(spread:pad, x1:0.028, y1:0.619, x2:1, y2:0.494, stop:0.852273 rgba(0, 0, 0, 250), stop:1 rgba(255, 255, 255, 255));")
+"")
         self.a40.setObjectName("a40")
         self.c50 = QtWidgets.QPushButton(self.centralwidget)
         self.c50.setGeometry(QtCore.QRect(380, 100, 31, 111))
         self.c50.setStyleSheet("background-color: rgb(0, 0, 0);\n"
-"background-color: qlineargradient(spread:pad, x1:0.028, y1:0.619, x2:1, y2:0.494, stop:0.852273 rgba(0, 0, 0, 250), stop:1 rgba(255, 255, 255, 255));")
+"")
         self.c50.setObjectName("c50")
         self.d50 = QtWidgets.QPushButton(self.centralwidget)
         self.d50.setGeometry(QtCore.QRect(420, 100, 31, 111))
         self.d50.setStyleSheet("background-color: rgb(0, 0, 0);\n"
-"background-color: qlineargradient(spread:pad, x1:0.028, y1:0.619, x2:1, y2:0.494, stop:0.852273 rgba(0, 0, 0, 250), stop:1 rgba(255, 255, 255, 255));")
+"")
         self.d50.setObjectName("d50")
         self.f50 = QtWidgets.QPushButton(self.centralwidget)
         self.f50.setGeometry(QtCore.QRect(500, 100, 31, 111))
-        self.f50.setStyleSheet("background-color: rgb(0, 0, 0);\n"
-"background-color: qlineargradient(spread:pad, x1:0.028, y1:0.619, x2:1, y2:0.494, stop:0.852273 rgba(0, 0, 0, 250), stop:1 rgba(255, 255, 255, 255));")
+        self.f50.setStyleSheet("background-color: rgb(0, 0, 0);")
         self.f50.setObjectName("f50")
         self.g50 = QtWidgets.QPushButton(self.centralwidget)
         self.g50.setGeometry(QtCore.QRect(540, 100, 31, 111))
         self.g50.setStyleSheet("background-color: rgb(0, 0, 0);\n"
-"background-color: qlineargradient(spread:pad, x1:0.028, y1:0.619, x2:1, y2:0.494, stop:0.852273 rgba(0, 0, 0, 250), stop:1 rgba(255, 255, 255, 255));\n"
-"text-color: rgb(225,225,225);")
+"")
         self.g50.setObjectName("g50")
         self.a50 = QtWidgets.QPushButton(self.centralwidget)
         self.a50.setGeometry(QtCore.QRect(590, 100, 31, 111))
         self.a50.setStyleSheet("background-color: rgb(0, 0, 0);\n"
-"background-color: qlineargradient(spread:pad, x1:0.028, y1:0.619, x2:1, y2:0.494, stop:0.852273 rgba(0, 0, 0, 250), stop:1 rgba(255, 255, 255, 255));\n"
-"text color: #fff")
+"")
         self.a50.setObjectName("a50")
+        self.comboBox = QtWidgets.QComboBox(self.centralwidget)
+        self.comboBox.setGeometry(QtCore.QRect(370, 40, 111, 22))
+        self.comboBox.setStyleSheet("background-color: rgb(174, 196, 255);")
+        self.comboBox.setObjectName("comboBox")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(250, 20, 261, 51))
+        self.label.setStyleSheet("font: 75 22pt \"MS Shell Dlg 2\";\n"
+"color: rgb(244, 224, 255);\n"
+"color: rgb(174, 196, 255);")
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(190, 360, 351, 51))
+        self.label_2.setStyleSheet("color: rgb(174, 196, 255);\n"
+"color: rgb(89, 142, 191);\n"
+"font: 16pt \"MS Shell Dlg 2\";\n"
+"color: rgb(192, 225, 99);\n"
+"color: rgb(154, 147, 153);\n"
+"color: rgb(174, 196, 255);\n"
+"\n"
+"")
+        self.label_2.setObjectName("label_2")
         self.c4.raise_()
         self.d4.raise_()
         self.e4.raise_()
@@ -174,6 +171,9 @@ class Ui_MainWindow(object):
         self.f50.raise_()
         self.g40.raise_()
         self.g50.raise_()
+        self.label.raise_()
+        self.label_2.raise_()
+        self.comboBox.raise_()
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 26))
@@ -184,6 +184,7 @@ class Ui_MainWindow(object):
         MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
+        self.comboBox.setCurrentIndex(-1)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
@@ -213,4 +214,8 @@ class Ui_MainWindow(object):
         self.f50.setText(_translate("MainWindow", "F5#"))
         self.g50.setText(_translate("MainWindow", "G5#"))
         self.a50.setText(_translate("MainWindow", "A5#"))
+        self.comboBox.setItemText(0, _translate("MainWindow", "Regular Piano"))
+        self.comboBox.setItemText(1, _translate("MainWindow", "Digital Piano"))
+        self.label.setText(_translate("MainWindow", "Piano "))
+        self.label_2.setText(_translate("MainWindow", "Please choose piano type"))
 
